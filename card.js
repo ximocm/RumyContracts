0af38c77d459fb925d259ad31eefd3a03b0d0665
Ximo Cabanes <69391516+ximocm@users.noreply.github.com>
2022-10-10 14:52:24 +0200

    Card.js fixed

--- card.js
+++ card.js
@@ -5,9 +5,18 @@ class Card{
 
         this.height = 32;
         this.width = 35;
+
+        if (suit = 'Joker'){
+            this.isSpecial = true;
+            this.filename = 'joker.png';
+        }
+        else{
+            this.isSpecial = false;
+            this.filename = number + suit + '.png';
+        }
     }
     display(){
-        //<img value={this.value} isSpecial={this.isSpecial?1:0} index={this.index} className="card-deck" src={'deck-images/'+this.filename}/>
+        <img value={this.value} isSpecial={this.isSpecial?1:0} index={this.index} className="card-deck" src={'images/'+this.filename}/>
     }
     getNum(){return this.number}
     getSuit(){return this.suit}
